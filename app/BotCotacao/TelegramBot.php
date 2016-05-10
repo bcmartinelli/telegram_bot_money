bafdb7913aed5820954a2f76a1111b08722b87a5
Martinelli <martinelli4630@gmail.com>
2016-05-10 16:24:53 -0300

    teste

---
+++ app/BotCotacao/TelegramBot.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\BotCotacao;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Curl\Curl;
+
+class TelegramBot
+{
+    private $url;
+    private $token_bot = '115524025:AAEGJD4EiE31ODPXlT1u-I0gAgKKKDQOpEg';
+
+    public function getUpdates()
+    {
+        $this->url = 'https://api.telegram.org/bot' . $this->$token_bot . '/getUpdates';
+
+        return $this->execute();
+    }
+
+    public function sendMessage($dolarAtual)
+    {
+        $this->url = 'https://api.telegram.org/bot' . $this->token_bot . '/sendMessage';
+        $data = [
+            'chat_id'    => '-33903501',
+            'text'       => 'Dolar: '. $dolarAtual,
+            'parse_mode' => 'Markdown',
+        ];
+        return $this->execute($data);
+    }
+
+    private function execute($data = null)
+    {
+        $curl = new Curl();
+        if($data !== null) {
+          echo $curl;
+            $result = $curl->post($this->url, $data);
+        } else {
+            $result = $curl->get($this->url);
+        }
+        $curl->close();
+
+        return $result;
+    }
+}
---
+++ app/Http/Controllers/CotacaoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\BotCotacao\CotacaoUol;
+use App\BotCotacao\TelegramBot;
+
+class CotacaoController extends Controller
+{
+    public function index() {
+      $uol = new CotacaoUol(); // criar uma instancia da classe
+
+      //receber os valores
+      list($dolarComercialCompra, $dolarComercialVenda, $dolarTurismoCompra, $dolarTurismoVenda, $euroCompra, $euroVenda, $libraCompra, $libraVenda, $pesosCompra, $pesosVenda) = $uol->pegaValores();
+
+      $tb = new TelegramBot();
+      $tb->sendMessage($dolarComercialVenda);
+
+      echo $dolarComercialVenda;
+    }
+}
