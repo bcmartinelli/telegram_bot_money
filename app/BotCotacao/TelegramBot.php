9bbc9b8e8b885d4adc9228848607a0596cf6e0f7
Bruno Martinelli <martinelli4630@gmail.com>
2016-05-12 22:16:37 -0300

    Teste mensagem automatica bot.

--- app/BotCotacao/TelegramBot.php
+++ app/BotCotacao/TelegramBot.php
@@ -9,10 +9,30 @@ use Curl\Curl;
 
 class TelegramBot
 {
+    define('BOT_TOKEN', '115524025:AAEGJD4EiE31ODPXlT1u-I0gAgKKKDQOpEg');
+    define('API_URL', 'https://api.telegram.org/bot115524025:AAEGJD4EiE31ODPXlT1u-I0gAgKKKDQOpEg/');
+
+    // read incoming info and grab the chatID
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chatID = $update["message"]["chat"]["id"];
+
+    // compose reply
+    $reply =  sendMessage();
+
+    // send reply
+    $sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+    file_get_contents($sendto);
+
+    function sendMessage(){
+    $message = "I am a baby bot.";
+    return $message;
+
+
     private $url;
     private $token_bot = '115524025:AAEGJD4EiE31ODPXlT1u-I0gAgKKKDQOpEg';
 
-    public function getMe()
+    /*public function getMe()
     {
         $this->url = 'https://api.telegram.org/bot' . $this->token_bot . '/getUpdates';
 
@@ -42,5 +62,5 @@ class TelegramBot
         $curl->close();
 
         return $result;
-    }
+    } */
 }
